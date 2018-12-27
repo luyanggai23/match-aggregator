6d4964c17133268e0e47da919cfaea800ab1f051
gail <luyanggai@gmail.com>
2018-12-27 15:23:34 -0800

    add acs endpoints to consts

--- constants.mjs
+++ constants.mjs
@@ -1,3 +1,4 @@
 export const API_KEY = 'RGAPI-6fddeee0-7fe4-45f5-b11d-11a50d795a53';
 export const HOST = 'https://na1.api.riotgames.com';
-// export const 
+export const ACS_HOST = 'https://acs.leagueoflegends.com';
+export const ACS_PLAYER_HISTORY_PATH = 'v1/stats/player_history/NA';
